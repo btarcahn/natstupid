4e710c4f91002b6e025f45897fbd53b63441d611
Bach Tran <batr.schip@gmail.com>
2020-03-12 15:25:13 +1100

    Broken code
    This code does not run, at all

--- prototype-01/game-artifacts/Board.py
+++ prototype-01/game-artifacts/Board.py
@@ -1,7 +1,20 @@
+import json
+
+
 class Board:
     """Represents the Board in the game."""
 
-    def __init__(self, m=0, n=0):
-        __x_size = m if m > 0 else 0
-        __y_size = n if n > 0 else m
-        self.__board = [[None for j in range(0, n)] for i in range(0, m)]
+    def __init__(self, json_string="", board_size=8):
+        """
+        Draws a board and initializes all tokens on this board, by
+        the directions given in the json string format.
+        :param json_string: the json string contains all token positions.
+        :param board_size: the size of the square board
+        """
+        self.__board = [[None for j in range(0, board_size)] for i in range(0, board_size)]
+
+        # Add tokens to the board according to the JSON directions
+        json_directions = json.loads(json_string)
+        for color in json_directions.keys():
+            # Ugh!!!
+            pass
